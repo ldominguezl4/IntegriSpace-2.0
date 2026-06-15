95e6913dc1bfb23225c33956e61e8c51b6dd750c
ldominguezl4 <ldominguezle@ucvvirtual.edu.pe>
2026-06-14 22:03:16 -0500

    Nuevos cambios aplicados

--- sw.js
+++ sw.js
@@ -1,20 +1,20 @@
 const CACHE_NAME = 'integrispace-cache-v2';
 const ASSETS = [
-  './index.html',
+  './pages/index.html',
   './pages/historial.html',
   './css/style.css',
   './css/historial.css',
   './assets/logo_integrispace.png'
 ];
 
-// Instalar la PWA y guardar archivos en el celular
+// Instalar la PWA y guardar archivos esenciales en el celular
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// Activar y limpiar archivos viejos
+// Activar y limpiar versiones viejas de la caché en el dispositivo
 self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then((keys) => {
@@ -27,7 +27,7 @@ self.addEventListener('activate', (e) => {
   );
 });
 
-// Hacer que la app funcione incluso sin internet
+// Permitir que cargue el contenido sin internet (Offline mode)
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     fetch(e.request).catch(() => caches.match(e.request))
